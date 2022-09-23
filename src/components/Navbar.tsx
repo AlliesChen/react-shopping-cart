bf91fa557d96c18ceeef40e046984f2e8be8884a
YPChen <cyphuaenng@pm.me>
2022-09-23 08:21:05 +0800

    update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,9 @@ export function Navbar() {
         </nav>
         <button className="group relative h-12 w-12 border-2 border-blue-100 rounded-full hover:bg-sky-500 hover:border-none">
           <ShoppingCartIcon className="mx-auto h-8 w-8 fill-sky-500 group-hover:fill-white" />
-          <div className="absolute right-0 bottom-0 translate-x-1/4 translate-y-1/4 h-6 w-6 bg-rose-500 text-rose-50 rounded-full">3</div>
+          <div className="absolute right-0 bottom-0 translate-x-1/4 translate-y-1/4 flex justify-center items-center h-6 w-6 bg-rose-500 text-rose-50 rounded-full">
+            3
+          </div>
         </button>
       </div>
     </header>
